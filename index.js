22d3728bdf5841af63e7cf943af3fc35287a1720
Cory Lynch <cjl2625@gmail.com>
2019-04-03 22:39:07 -0400

    Add decrypting basic values.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const chalk = require("chalk");
 const shell = require("shelljs");
 const request = require("request-promise-native");
 const seal = require('./sealjs');
+const fs = require('fs');
 
 const AGG_URI = "http://localhost:5000";
 
@@ -37,6 +38,22 @@ const keygen = async () => {
   sk.save('secret.key');
 
   console.log(chalk.bold.green("Saved keys to this directory"));
+};
+
+const decrypt = (path) => {
+  const context = new seal.SEALContext(2048, 128, 65536);
+
+  // Generate a public/secret key pair
+  const keygen = new seal.KeyGenerator(context);
+  const sk = new seal.SecretKey('secret.key');
+
+  const ciphertext = new seal.Ciphertext(path);
+
+  const decryptor = new seal.Decryptor(context, sk);
+  const result = new seal.Plaintext(decryptor, ciphertext);
+
+  const encoder = new seal.Encoder(65536);
+  return encoder.decode(result);
 };
 
 const run = async () => {
@@ -78,13 +95,20 @@ const run = async () => {
 
   const req = {
     uri: AGG_URI + "/api/aggregate", 
+    encoding: null,
     qs: {
       study: studies[choice]._id
     }
   };
-  const result = await request(req);
-  console.log(result);
-  
+
+  console.log(chalk.gray("Aggregating..."));
+  const valueData = await request(req)
+  await fs.writeFileSync('value.seal', valueData);
+
+  console.log(chalk.green("Done! Now decrypting..."));
+
+  const value = decrypt('value.seal');
+  console.log(chalk.green("Result: " + chalk.bold(value)));
 };
 
 run();
